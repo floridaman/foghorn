2a561d7f6654bea2d34681daee0c887dd72732e2
James Daniels <danielsj9@gmail.com>
2024-02-19 16:10:18 -0600

    Initial commit

---
+++ client/client.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"hash/crc32"
+	"net"
+	"os"
+	"sort"
+)
+
+type Block struct {
+	Index    uint32
+	Data     []byte
+	Checksum uint32
+}
+
+func main() {
+	multicastAddress := flag.String("address", "224.3.29.71:10000", "Multicast address and port")
+	savePath := flag.String("save", "received_file", "Path to save the received file")
+	flag.Parse()
+
+	addr, err := net.ResolveUDPAddr("udp", *multicastAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := net.ListenMulticastUDP("udp", nil, addr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	blocks := make(map[uint32]Block)
+	var totalBlocks uint32
+
+	buffer := make([]byte, 4096+8)
+	fmt.Println("Listening for multicast messages...")
+
+	for {
+		n, _, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			panic(err)
+		}
+
+		blockIndex := binary.BigEndian.Uint32(buffer[:4])
+		receivedChecksum := binary.BigEndian.Uint32(buffer[4:8])
+		data := buffer[8:n]
+
+		calculatedChecksum := crc32.ChecksumIEEE(data)
+		if receivedChecksum != calculatedChecksum {
+			fmt.Printf("Checksum mismatch for block %d.\n", blockIndex)
+			continue
+		}
+
+		if blockIndex == 0 {
+			totalBlocks = binary.BigEndian.Uint32(data[:4])
+			fmt.Printf("Primary block received. Total Blocks: %d\n", totalBlocks)
+		} else {
+			blocks[blockIndex] = Block{Index: blockIndex, Data: data, Checksum: receivedChecksum}
+		}
+
+		if uint32(len(blocks)) == totalBlocks-1 {
+			writeBlocksInOrder(blocks, *savePath, totalBlocks)
+			break
+		}
+	}
+}
+
+func writeBlocksInOrder(blocks map[uint32]Block, savePath string, totalBlocks uint32) {
+	file, err := os.Create(savePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var indices []int
+	for index := range blocks {
+		indices = append(indices, int(index))
+	}
+	sort.Ints(indices)
+
+	for _, index := range indices {
+		block := blocks[uint32(index)]
+		_, err := file.Write(block.Data)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Println("File successfully reconstructed and saved.")
+}
---
+++ server/server.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+    "encoding/binary"
+    "flag"
+    "fmt"
+    "hash/crc32"
+    "io"
+    "net"
+    "os"
+    "time"
+)
+
+// Block represents a data block to be sent.
+type Block struct {
+    Index    uint32
+    Checksum uint32
+    Data     []byte
+}
+
+func main() {
+    // Command line arguments
+    filePath := flag.String("file", "path/to/your/file", "Path to the file to be sent")
+    multicastAddress := flag.String("address", "224.3.29.71:10000", "Multicast address and port")
+    blockSize := flag.Int("size", 4096, "Block size in bytes")
+
+    flag.Parse()
+
+    // Open file
+    file, err := os.Open(*filePath)
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+
+    fileInfo, err := file.Stat()
+    if err != nil {
+        panic(err)
+    }
+
+    // Calculate total blocks (+2 for possible partial block and primary block)
+    fileSize := fileInfo.Size()
+    totalBlocks := int(fileSize)/(*blockSize) + 2
+
+    // Calculate file checksum
+    hash := crc32.NewIEEE()
+    if _, err := file.Seek(0, 0); err != nil { // Seek to start
+        panic(err)
+    }
+    if _, err := io.Copy(hash, file); err != nil {
+        panic(err)
+    }
+    fileChecksum := hash.Sum32()
+
+    // Prepare primary block
+    primaryBlock := Block{
+        Index:    0, // Primary block index 0
+        Checksum: fileChecksum,
+        Data:     make([]byte, 12), // Placeholder for totalBlocks and fileChecksum
+    }
+    binary.BigEndian.PutUint32(primaryBlock.Data[0:4], uint32(totalBlocks))
+    binary.BigEndian.PutUint64(primaryBlock.Data[4:12], uint64(fileChecksum))
+
+    // Setup UDP connection
+    addr, err := net.ResolveUDPAddr("udp", *multicastAddress)
+    if err != nil {
+        panic(err)
+    }
+    conn, err := net.DialUDP("udp", nil, addr)
+    if err != nil {
+        panic(err)
+    }
+    defer conn.Close()
+
+    fmt.Printf("Sending file: %s\n", *filePath)
+    fmt.Printf("Multicast Address: %s\n", *multicastAddress)
+    fmt.Printf("Block Size: %d bytes\n", *blockSize)
+
+    // Infinite loop to send file blocks
+    for {
+        // Send primary block
+        sendBlock(conn, primaryBlock)
+
+        // Reset file pointer to beginning for block transmission
+        if _, err := file.Seek(0, 0); err != nil {
+            panic(err)
+        }
+
+        // Block transmission
+        buffer := make([]byte, *blockSize)
+        blockIndex := uint32(1) // Start from 1, 0 is for primary block
+        for {
+            bytesRead, err := file.Read(buffer)
+            if err == io.EOF {
+                break
+            }
+            if err != nil {
+                panic(err)
+            }
+
+            // Calculate block checksum
+            hash.Reset()
+            hash.Write(buffer[:bytesRead])
+            blockChecksum := hash.Sum32()
+
+            // Prepare and send block
+            block := Block{
+                Index:    blockIndex,
+                Checksum: blockChecksum,
+                Data:     buffer[:bytesRead],
+            }
+            sendBlock(conn, block)
+            blockIndex++
+        }
+
+        // Delay between each file transmission
+        time.Sleep(1 * time.Second)
+    }
+}
+
+// sendBlock sends a block over UDP.
+func sendBlock(conn *net.UDPConn, block Block) {
+    header := make([]byte, 8)
+    binary.BigEndian.PutUint32(header[0:4], block.Index)
+    binary.BigEndian.PutUint32(header[4:8], block.Checksum)
+    if _, err := conn.Write(append(header, block.Data...)); err != nil {
+        panic(err)
+    }
+}
