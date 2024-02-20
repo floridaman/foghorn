90abae9e0c7d2a68794715d7d7304601c565657f
James Daniels <danielsj9@gmail.com>
2024-02-19 20:53:21 -0600

    Update server to read once

--- server/server.go
+++ server/server.go
@@ -5,8 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"hash/crc32"
-	"io"
-	"math"
 	"net"
 	"os"
 	"time"
@@ -14,54 +12,45 @@ import (
 
 // Block represents a data block to be sent.
 type Block struct {
-	Index    uint32
-	Checksum uint32
-	Data     []byte
+	Index       int
+	TotalBlocks int
+	Hash        uint32
+	Data        []byte
 }
 
 func main() {
 	// Command line arguments
-	filePath := flag.String("file", "path/to/your/file", "Path to the file to be sent")
+	filePath := flag.String("file", "", "Path to the file to be sent")
 	multicastAddress := flag.String("address", "224.3.29.71:10000", "Multicast address and port")
 	blockSize := flag.Int("size", 508, "Block size in bytes")
 	delay := flag.Int("delay", 100, "Delay in ms")
 
 	flag.Parse()
 
-	// Open file
-	file, err := os.Open(*filePath)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	fileInfo, err := file.Stat()
-	if err != nil {
-		panic(err)
+	if *filePath == "" {
+		fmt.Println("File path is required")
+		return
 	}
 
-	// Calculate total blocks (+2 for possible partial block and primary block)
-	fileSize := fileInfo.Size()
-	totalBlocks := math.Ceil(float64(fileSize) / float64(*blockSize))
+	// Read the entire file into memory
+	fileData, err := os.ReadFile(*filePath)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
+	}
+
+	// Calculate total blocks
+	totalBlocks := (len(fileData) + *blockSize - 1) / *blockSize
 
 	// Calculate file checksum
-	hash := crc32.NewIEEE()
-	if _, err := file.Seek(0, 0); err != nil { // Seek to start
-		panic(err)
-	}
-	if _, err := io.Copy(hash, file); err != nil {
-		panic(err)
-	}
-	fileChecksum := hash.Sum32()
+	hash := crc32.ChecksumIEEE(fileData)
 
 	// Prepare primary block
 	primaryBlock := Block{
-		Index:    0, // Primary block index 0
-		Checksum: fileChecksum,
-		Data:     make([]byte, 12), // Placeholder for totalBlocks and fileChecksum
+		Index:       0,
+		TotalBlocks: totalBlocks,
+		Hash:        hash,
 	}
-	binary.BigEndian.PutUint32(primaryBlock.Data[0:4], uint32(totalBlocks))
-	binary.BigEndian.PutUint64(primaryBlock.Data[4:12], uint64(fileChecksum))
 
 	// Setup UDP connection
 	addr, err := net.ResolveUDPAddr("udp4", *multicastAddress)
@@ -78,7 +67,7 @@ func main() {
 	fmt.Printf("Multicast Address: %s\n", *multicastAddress)
 	fmt.Printf("Block Size: %d bytes\n", *blockSize)
 
-	currentBlock := uint32(0)
+	currentBlock := 0
 
 	// Start a goroutine for status updates
 	go func() {
@@ -93,33 +82,18 @@ func main() {
 		// Send primary block
 		sendBlock(conn, primaryBlock)
 
-		// Reset file pointer to beginning for block transmission
-		if _, err := file.Seek(0, 0); err != nil {
-			panic(err)
-		}
+		// blockIndex := uint32(1) // Start from 1, 0 is for primary block
 
-		// Block transmission
-		buffer := make([]byte, *blockSize)
-		blockIndex := uint32(1) // Start from 1, 0 is for primary block
-		for {
-			bytesRead, err := file.Read(buffer)
-			if err == io.EOF {
-				break
+		for blockIndex := 1; blockIndex < len(fileData); blockIndex += *blockSize {
+			endIndex := blockIndex + *blockSize
+			if endIndex > len(fileData) {
+				endIndex = len(fileData)
 			}
-			if err != nil {
-				panic(err)
-			}
-
-			// Calculate block checksum
-			// hash.Reset()
-			// hash.Write(buffer[:bytesRead])
-			// blockChecksum := hash.Sum32()
 
 			// Prepare and send block
 			block := Block{
 				Index: blockIndex,
-				// Checksum: blockChecksum,
-				Data: buffer[:bytesRead],
+				Data:  fileData[blockIndex:endIndex],
 			}
 			sendBlock(conn, block)
 			blockIndex++
@@ -133,9 +107,9 @@ func main() {
 
 // sendBlock sends a block over UDP.
 func sendBlock(conn *net.UDPConn, block Block) {
-	header := make([]byte, 8)
-	binary.BigEndian.PutUint32(header[0:4], block.Index)
-	binary.BigEndian.PutUint32(header[4:8], block.Checksum)
+	header := make([]byte, 4)
+	binary.BigEndian.PutUint32(header[0:4], uint32(block.Index))
+
 	if _, err := conn.Write(append(header, block.Data...)); err != nil {
 		panic(err)
 	}
